90feaa1fd3ada3ce17ba8ec6acac0c51757b9a3c
Marcanius <Mattieplatenburg@gmail.com>
2015-05-17 22:44:40 +0200

    Start Opdracht 4

--- Opdracht4/Program.cs
+++ Opdracht4/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Opdracht4
+// Naam: Matthijs Platenburg
+// Studentnummer: 4260953
+
+class Program
 {
-    class Program
+    static long p, q, s, m, x;
+    static long result;
+    static long[] array;
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Parsing input.
+        p = long.Parse(Console.ReadLine());
+        q = long.Parse(Console.ReadLine());
+        s = long.Parse(Console.ReadLine());
+        m = long.Parse(Console.ReadLine());
+        x = long.Parse(Console.ReadLine());
+
+        // Creating the array.
+        array = new long[m / x];
+
+        array[0] = s;
+
+        for (int i = 1; i < m; i++)
         {
+            array[i] = (p * array[i - 1] + q) % m;
+
+            // if the difference with another value in the array is smaller than x, or larger than m-x, retrun the result;
+            if ()
+            result = i + 1;
+            break;
         }
+        // Printing the result.
+        Console.WriteLine(result);
     }
 }
