f849226c2b7eaa2b5ba067aed76db487ecc71013
Marcanius <Mattieplatenburg@gmail.com>
2015-06-07 19:24:09 +0200

    Start Opdracht 6

---
+++ Opdracht6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+// Naam: Matthijs Platenburg
+// Studentnummer: 4260953
+
+// TODO:
+/* 
+ * Input
+ * Output
+ * Dynamic Array for output
+ * Objects for Players
+ * Hash Function
+ * Query Calculation
+ *  
+ */
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup the hash table.
+
+        /* 
+           Loop Start
+        // Recieve input.
+
+
+        // Add input to the output.
+           
+           Loop Stop
+        */
+
+        // Output.
+    }
+}
