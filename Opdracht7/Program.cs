1056554991a1a764a29059256debd61ea33bfd1f
Marcanius <Mattieplatenburg@gmail.com>
2015-06-09 21:53:52 +0200

    Start Opdracht 7
    What is red black tree?

---
+++ Opdracht7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+// Naam: Matthijs Platenburg
+// Studentnummer: 4260953
+
+// Soort Boom: TODO
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup.
+
+
+        string input;
+        string[] inputSplit;
+        
+        // Loop through instructions.
+        input = Console.ReadLine();
+        while (input != null && input != "")
+        {
+            inputSplit = input.Split(' ');
+
+
+            // Recieve next input.
+            input = Console.ReadLine();
+        }
+    }
+}
+
+class Speler
+{
+    int PlayerNumber;
+    int Booms;
+
+    public Speler(int PlayerNumber, int Booms)
+    {
+        this.PlayerNumber = PlayerNumber;
+        this.Booms = PlayerNumber;
+    }
+}
+
+class Tree
+{
+
+}
