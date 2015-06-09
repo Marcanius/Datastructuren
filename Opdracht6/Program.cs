f62bc67fac2858fb3ba044c0973a9c2b6516d7ac
Matthijs Platenburg <Mattieplatenburg@gmail.com>
2015-06-09 10:12:09 +0200

    Progress Opdracht 6
    Slowly but surely

--- Opdracht6/Program.cs
+++ Opdracht6/Program.cs
@@ -23,16 +23,77 @@ class Program
     {
         // Setup the hash table.
 
-        /* 
-           Loop Start
-        // Recieve input.
+        // Loop Start
+        string input;
+        string[] inputSplit;
+        input = Console.ReadLine();
+        while (input != null)
+        {
+            inputSplit = input.Split(' ');
 
+            // Calculate correct output.
+            // New Player.
+            if (inputSplit[0] == "N")
+                OutputN(CalculateHash(inputSplit[1]), inputSplit[1]);
 
-        // Add input to the output.
-           
-           Loop Stop
-        */
+            // Delete Player
+            else if (inputSplit[0] == "D")
+                OutputD(CalculateHash(inputSplit[1]), inputSplit[1]);
 
-        // Output.
+            // Output
+
+            // Recieve next input.
+            input = Console.ReadLine();
+        }
+    }
+
+    static uint CalculateHash(string Input)
+    {
+        uint result = 0;
+
+        // Calculate the hash.
+        for (int i = 0; i < Input.Length; i++)
+            result += (uint)i * Input[i] * Input[i];
+
+        // Return the calculated hash.
+        return result;
+    }
+
+    // Input a new Player.
+    static void OutputN(uint Hash, string Name)
+    {
+
+    }
+
+    // Delete an existing player.
+    static void OutputD()
+    {
+
+    }
+
+    // Raise the score of an existing player.
+    static void OutputX()
+    {
+
+    }
+
+    // Print the score of an existing player.
+    static void OutputQ()
+    {
+
+    }
+}
+
+class Speler
+{
+    public Speler previous, next;
+    public readonly string name;
+    public uint score;
+
+    public Speler(string Name, uint Score, Speler Previous = null)
+    {
+        this.name = Name;
+        this.score = Score;
+        this.previous = Previous;
     }
 }
