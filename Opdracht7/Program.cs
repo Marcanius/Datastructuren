39bb1745e57adb2610ac307c693ddb7657b9be4c
Marcanius <Mattieplatenburg@gmail.com>
2015-06-09 22:06:55 +0200

    Progress Opdracht 7
    Still dunno what search trees are
    I'll find out and report back

--- Opdracht7/Program.cs
+++ Opdracht7/Program.cs
@@ -15,17 +15,45 @@ class Program
 
         string input;
         string[] inputSplit;
-        
+
         // Loop through instructions.
         input = Console.ReadLine();
         while (input != null && input != "")
         {
             inputSplit = input.Split(' ');
+            string instruction = inputSplit[0];
+            int playerNumber = int.Parse(inputSplit[1]);
 
+            // Add new Player.
+            if (instruction == "T")
+                AddNewPlayer(playerNumber, int.Parse(inputSplit[2]));
+
+            // Print part of the tree.
+            if (instruction == "G")
+                PrintRange(playerNumber);
+
+            // Player's rank.
+            if (instruction == "R")
+                PlayerRank(playerNumber);
 
             // Recieve next input.
             input = Console.ReadLine();
         }
+    }
+
+    static void AddNewPlayer(int PlayerNumber, int PlayerScore)
+    {
+
+    }
+
+    static void PrintRange(int PlayerNumber)
+    {
+
+    }
+
+    static void PlayerRank(int PlayerNumber)
+    {
+
     }
 }
 
